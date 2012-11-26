6e18f41511b8dc587b3c6878418fb0dafb24b0d2
Alex Farquhar <alex.farquhar@forward.co.uk>
2012-11-26 14:57:31 +0000

    dont load all prefs at startup

--- core/app/models/spree/preferences/store.rb
+++ core/app/models/spree/preferences/store.rb
@@ -1,9 +1,3 @@
-# Use singleton class Spree::Preferences::Store.instance to access
-#
-# StoreInstance has a persistence flag that is on by default,
-# but we disable database persistence in testing to speed up tests
-#
-
 require 'singleton'
 
 module Spree::Preferences
@@ -12,32 +6,51 @@ module Spree::Preferences
     attr_accessor :persistence
 
     def initialize
+      # puts "AF: persist initialize"
       @cache = Rails.cache
-      @persistence = true
+      @persistence = false
       load_preferences
     end
 
     def set(key, value, type)
+      # puts "AF: set #{key} #{value} #{type}"
       @cache.write(key, value)
       persist(key, value, type)
     end
 
     def exist?(key)
-      @cache.exist? key
+      # puts "AF: exist? #{key}"
+      @cache.exist?(key) || !!try_db(key)
     end
 
+
     def get(key)
-      @cache.read(key)
+      try_db(key)
     end
 
     def delete(key)
+      # puts "AF: delete #{key}"
       @cache.delete(key)
       destroy(key)
     end
 
     private
 
+
+    def try_db(key)
+      val = @cache.read(key)
+      if(!val)
+        from_db = Spree::Preference.where(:key => key).first
+        if(from_db)
+          @cache.write(key, from_db.value)
+          val = from_db.value
+        end
+      end
+      val
+    end
+
     def persist(cache_key, value, type)
+      # puts "AF: persist #{cache_key} #{value} #{type}"
       return unless should_persist?
 
       preference = Spree::Preference.find_or_initialize_by_key(cache_key)
@@ -47,6 +60,7 @@ module Spree::Preferences
     end
 
     def destroy(cache_key)
+      # puts "AF: destroy #{cache_key}"
       return unless should_persist?
 
       preference = Spree::Preference.find_by_key(cache_key)
@@ -54,10 +68,12 @@ module Spree::Preferences
     end
 
     def load_preferences
+      # puts "AF: load prefs"
       return unless should_persist?
 
       Spree::Preference.valid.each do |p|
         Spree::Preference.convert_old_value_types(p) # see comment
+        # puts "AF: write #{p.key} #{p.value}"
         @cache.write(p.key, p.value)
       end
     end
